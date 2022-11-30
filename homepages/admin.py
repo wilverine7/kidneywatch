8a525fefbff1ee11b78a84f599949dcc11866994
cadegardanier <cade.gardanier@gmail.com>
2022-11-30 11:45:28 -0700

    models

--- homepages/admin.py
+++ homepages/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import comorbidity_type, person, daily_log, meal_type, substance, meal
+
+admin.site.register(comorbidity_type)
+admin.site.register(person)
+admin.site.register(daily_log)
+admin.site.register(meal_type)
+admin.site.register(substance)
+admin.site.register(meal)
 
 # Register your models here.
